75b739fd1eabb3152d8b3aeb759a4f79b1d8f817
mcooling <marccooling@outlook.com>
2022-02-02 13:55:03 +0000

    Add ESLint configuration
    Configure .eslintrc.js with AirBnB style guide and Prettier plugin
    Add common ignore rules

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": 0,
+    "react/jsx-filename-extension": [0],
+    "no-use-before-define": [
+      "error",
+      { functions: true, classes: true, variables: false },
+    ],
+  },
+};
