d4986567fa0b1f5cabd928eb28197f32b670c4c7
mcooling <marccooling@outlook.com>
2022-02-23 22:07:07 +0000

    Update Login screen, cosmetic changes

--- src/components/01_LoginSignUp/Login.js
+++ src/components/01_LoginSignUp/Login.js
@@ -1,31 +1,16 @@
-import React from "react";
-import { Text, TouchableOpacity, View } from "react-native-web";
-import { TextInput } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import GlobalStyles from "../../utils/GlobalStyles";
-
-// see wk3 ex5 for text input / state
-// implements POST/user endpoint
-// api url http://localhost:3333/api/1.0.0/
-
-// object to store authentication token
-// todo think i might be able to replace this with import - see home/logout
-const authToken = async (value) => {
-  try {
-    const jsonValue = JSON.stringify(value);
-    await AsyncStorage.setItem("@auth_token", jsonValue);
-  } catch (error) {
-    console.error(error);
-  }
-};
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native-web';
+import { TextInput } from 'react-native';
+import GlobalStyles from '../../utils/GlobalStyles';
+import { setAuthToken, setUserId } from '../../utils/AsyncStorage';
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      emailAddress: "thom@yorke.com",
-      password: "hello123",
+      emailAddress: 'marc@cooling.com',
+      password: 'hello123',
     };
   }
 
@@ -33,36 +18,41 @@ class Login extends React.Component {
 
   // function called by login button
   loginUser = () => {
-    return fetch("http://localhost:3333/api/1.0.0/login/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // note to self: left-hand key must match the API spec
-      body: JSON.stringify({
-        email: this.state.emailAddress,
-        password: this.state.password,
-      }),
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        console.log("Login successful");
-        authToken(json).then(() => {
-          this.props.navigation.navigate("Home");
-        });
+    return (
+      fetch('http://localhost:3333/api/1.0.0/login/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        // note to self: left-hand key must match the API spec
+        body: JSON.stringify({
+          email: this.state.emailAddress,
+          password: this.state.password,
+        }),
       })
-      .catch((error) => {
-        // todo may want to display a modal / alert here?
-        console.log(`Login unsuccessful: ${error}`);
-      });
+        // todo do i need to chain setters in this way? looks confusing
+        .then((response) => response.json())
+        .then((json) => {
+          console.log('Login successful');
+          console.log(json);
+          setUserId(json.id).then(() => {
+            setAuthToken(json.token).then(() => {
+              this.props.navigation.navigate('Main');
+            });
+          });
+        })
+        .catch((error) => {
+          // todo may want to display a modal / alert here?
+          console.log(`Login unsuccessful: ${error}`);
+        })
+    );
   };
 
   render() {
     return (
       <View>
-        <View style={GlobalStyles.container}>
-          <Text style={GlobalStyles.screenTitle}>LOGIN PAGE</Text>
+        <View style={GlobalStyles.headerContainer}>
+          <Text style={GlobalStyles.screenTitle}>LOGIN</Text>
         </View>
         <TextInput
           placeholder="Email Address: "
@@ -77,22 +67,24 @@ class Login extends React.Component {
           style={GlobalStyles.textInput}
           secureTextEntry
         />
-        <TouchableOpacity
-          style={GlobalStyles.button}
-          onPress={() => {
-            this.loginUser();
-          }}
-        >
-          <Text style={GlobalStyles.buttonText}>LOGIN</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={GlobalStyles.button}
-          onPress={() => {
-            this.props.navigation.navigate("SignUp");
-          }}
-        >
-          <Text style={GlobalStyles.buttonText}>SIGN UP</Text>
-        </TouchableOpacity>
+        <View style={GlobalStyles.smallButtonContainer}>
+          <TouchableOpacity
+            style={GlobalStyles.smallButton}
+            onPress={() => {
+              this.loginUser();
+            }}
+          >
+            <Text style={GlobalStyles.buttonText}>LOGIN</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={GlobalStyles.smallButton}
+            onPress={() => {
+              this.props.navigation.navigate('SignUp');
+            }}
+          >
+            <Text style={GlobalStyles.buttonText}>SIGN UP</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
