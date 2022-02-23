bd8ac3170687aa8a2ca6a6d610369f88c449d3f6
mcooling <marccooling@outlook.com>
2022-02-23 22:57:46 +0000

    Add ESLint, fixed broken config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,24 +4,24 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "airbnb",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {
-    "react/react-in-jsx-scope": 0,
-    "react/jsx-filename-extension": [0],
-    "no-use-before-define": [
-      "error",
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': [0],
+    'no-use-before-define': [
+      'error',
       { functions: true, classes: true, variables: false },
     ],
   },
