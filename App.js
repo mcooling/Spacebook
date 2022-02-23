95a9e12b31bc4c3bfb0437826affc2320eef1d51
mcooling <marccooling@outlook.com>
2022-02-23 22:01:29 +0000

    Update App.js screen with navigation changes
    Update App.js to reflect new navigation structure. Reduced to three stack screens, rest replaced with tab navigation.

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
-import React from "react";
+import React from 'react';
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import SignUp from "./src/components/01_LoginSignUp/SignUp";
-import Login from "./src/components/01_LoginSignUp/Login";
-import AddPost from "./src/components/02_PostMgmt/AddPost";
-import FriendPost from "./src/components/02_PostMgmt/FriendPost";
-import MyPost from "./src/components/02_PostMgmt/MyPost";
-import MyFriends from "./src/components/03_FriendMgmt/MyFriends";
-import AddFriends from "./src/components/03_FriendMgmt/AddFriends";
-import FriendProfile from "./src/components/03_FriendMgmt/FriendProfile";
-import MyDetails from "./src/components/04_ProfileMgmt/MyDetails";
-import MyProfile from "./src/components/04_ProfileMgmt/MyProfile";
-import Home from "./src/components/Home";
+import SignUp from './src/components/01_LoginSignUp/SignUp';
+import Login from './src/components/01_LoginSignUp/Login';
+import TabNavigator from './src/navigators/TabNavigator';
 
 const Stack = createNativeStackNavigator();
 
 class App extends React.Component {
   render() {
+    // todo check in with ash on nav structures
     return (
       <NavigationContainer>
         <Stack.Navigator
@@ -27,17 +20,9 @@ class App extends React.Component {
             headerShown: false,
           }}
         >
-          <Stack.Screen name="SignUp" component={SignUp} />
           <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="AddPost" component={AddPost} />
-          <Stack.Screen name="FriendPost" component={FriendPost} />
-          <Stack.Screen name="MyPost" component={MyPost} />
-          <Stack.Screen name="MyFriends" component={MyFriends} />
-          <Stack.Screen name="AddFriends" component={AddFriends} />
-          <Stack.Screen name="FriendProfile" component={FriendProfile} />
-          <Stack.Screen name="MyDetails" component={MyDetails} />
-          <Stack.Screen name="MyProfile" component={MyProfile} />
-          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="Main" component={TabNavigator} />
         </Stack.Navigator>
       </NavigationContainer>
     );
