dee42c6d2f2f6747d328bf01bfa3758332365dbe
mcooling <marccooling@outlook.com>
2022-03-08 18:28:17 +0000

    Update AsyncStorage, to support draft post functionality
    Add four new functions, to support get, add, update and delete draft post functionality.

--- src/utils/AsyncStorage.js
+++ src/utils/AsyncStorage.js
@@ -84,3 +84,115 @@ export const getPostId = async () => {
     console.error(error);
   }
 };
+
+// adds profile post to draft
+// each post pushed to an array
+
+// add first post - calls addDraftPost (setItem)
+
+export const addDraftPost = async (postText) => {
+  try {
+    // get the existing array
+    const draftPostArray = JSON.parse(
+      await AsyncStorage.getItem('@draft_post')
+    );
+    // console.log(draftPostArray);
+    // console.log(postText);
+
+    // create new json object to be added
+    // todo think i may need more logic here
+    // deleting a record doesn't update other ids
+    // so post id's no longer match element id for new posts
+    // tries to create new record with an id that already exists
+
+    // need to check array for post id values each time?
+
+    const postObject = { id: draftPostArray.length, post: postText };
+    console.log(postObject);
+
+    // if item in get, append with the new thing i'm creating, then resend
+    // nothing, ned to create first item
+    // get current array from async storage - get what's currently stored
+    // add new post to end of array
+    // add back into async storage
+    // need to parse object to json. check the length
+    // new id = json.length
+    // don't need array.push anymore
+
+    // push new object into existing array
+    draftPostArray.push(postObject);
+
+    // update async storage with new array
+    await AsyncStorage.setItem('@draft_post', JSON.stringify(draftPostArray));
+    console.log(draftPostArray);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// gets post from draft post array
+export const getDraftPost = async () => {
+  try {
+    const getPostArray = await AsyncStorage.getItem('@draft_post');
+
+    if (getPostArray != null) {
+      // we have draft posts
+      // console.log(JSON.parse(getPostArray));
+      return JSON.parse(getPostArray); // converts string to json object
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// removes post from list of drafts
+export const deleteDraftPost = async (postId) => {
+  try {
+    // get the existing array
+    const draftPostArray = JSON.parse(
+      await AsyncStorage.getItem('@draft_post')
+    );
+    console.log(draftPostArray);
+    console.log(`Post id to be deleted: ${postId}`);
+
+    draftPostArray.splice(postId, 1);
+
+    // reindex array elements after deleting an item
+    while (postId < draftPostArray.length) {
+      draftPostArray[postId].id--;
+      postId++;
+    }
+
+    // update async storage with new array
+    await AsyncStorage.setItem('@draft_post', JSON.stringify(draftPostArray));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// update draft post
+// todo not got this working quite right
+export const updateDraftPost = async (draftPost, draftPostId) => {
+  try {
+    // get the existing array
+    const draftPostArray = JSON.parse(
+      await AsyncStorage.getItem('@draft_post')
+    );
+    console.log(draftPostArray);
+    console.log(
+      `Update post id ${draftPostId} with new text value ${draftPost}`
+    );
+
+    // todo console error
+    // draftPostArray.update(postId, postText);
+    // draftPostArray.postId = postText;
+    draftPostArray[draftPostId].post = draftPost;
+
+    // draftPostArray.splice(postId, 1);
+
+    // update async storage with new array
+    await AsyncStorage.setItem('@draft_post', JSON.stringify(draftPostArray));
+  } catch (error) {
+    console.error(error);
+  }
+};
